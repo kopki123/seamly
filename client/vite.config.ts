eeb58776dd7b0d97bf18471a82e180005a3314c7
kopki <kopki684@gmail.com>
2025-03-25 00:39:55 +0800

    fix: 不使用環境變數

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
